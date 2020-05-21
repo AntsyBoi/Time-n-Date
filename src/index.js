f1acf50125e563541708698d4c06855c591033fc
Ant <Ant@users.noreply.github.com>
2020-05-20 20:49:46 -0500

    Changed the window from 800x600 to 800x800

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 800,
   });
 
   // and load the index.html of the app.
