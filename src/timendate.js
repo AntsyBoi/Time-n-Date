8099a02df08231282acf957a7f23a3adeae74c4a
Ant <Ant@users.noreply.github.com>
2020-05-22 23:54:29 -0500

    Removed unnecessary semicolons

--- src/timendate.js
+++ src/timendate.js
@@ -15,7 +15,7 @@ function draw() {
     var d = day()
     
     if (hour() > 12) {
-        preh = hour() - 12;
+        preh = hour() - 12
     }
     var h = preh
     if (preh < 9 && preh = 9) {
@@ -40,14 +40,14 @@ function draw() {
     
     //Global section
     background(40) //Sets the background color for the project
-    fill(200); //Sets the color for all of the text.
+    fill(200) //Sets the color for all of the text.
     //Credits section
-    textAlign(CENTER, TOP); //Aligns the text for the credits
+    textAlign(CENTER, TOP) //Aligns the text for the credits
     textSize(windowHeight / 39.88) //Sizes the text for the credits
     text("Created by Charles Rinehart", 0, windowHeight / 99.7, width) //Displays the credits
     //Time and date section
-    textSize(windowHeight / 7.976); //Defines size for the text of the time and date
-    textAlign(CENTER, CENTER); //Aligns the text
+    textSize(windowHeight / 7.976) //Defines size for the text of the time and date
+    textAlign(CENTER, CENTER) //Aligns the text
     text(h + ":" + m + ":" + s, 0, windowHeight / 2 - (windowHeight / 7.976 / 2.08333333333), width) //Displays the current time
     text(d + " / " + mo + " / " + year(), 0, windowHeight / 2 + (windowHeight / 7.976 / 2.08333333333), width) //Displays the current date
     //Timer section
