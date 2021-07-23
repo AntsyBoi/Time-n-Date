acfad6c3ad5704f6b8465338cc4db1669bc68671
Ant <38162305+AntsyBoi@users.noreply.github.com>
2021-07-23 18:19:35 -0500

    HOW IS IT BACK
    HOW IS IT BACK I HATE ALL OF THIS

---
+++ README.md
@@ -0,0 +1,7 @@
+
+# Time-n-Date
+A simple clock web app made with p5
+
+# Compiling
+**Node.js is required to install.**
+Go into the source code's folder from your terminal of choice and then run `npm install` then enjoy!
---
+++ timendate.js
@@ -0,0 +1,52 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+/** USE 997 WHEN DECODING THE DIVISION*/
+function draw() {
+    resizeCanvas(windowWidth, windowHeight);
+//Sets up variables so then they will display 0s when displaying
+    var preh = hour()
+    var m = minute()
+    var mo = month()
+    var s = second()
+    var d = day()
+//Declares if they will have a 0 before the number
+    if (hour() > 12) {
+        preh = hour() - 12
+    }
+    var h = preh
+    if (preh < 10) {
+        h = "0" + preh
+    }
+
+    if (minute() < 10) {
+        m = "0" + minute()
+    }
+
+    if (month() < 10) {
+        mo = "0" + month()
+    }
+
+    if (second() < 10) {
+        s = "0" + second()
+    }
+
+    if (day() < 10) {
+        d = "0" + day()
+    }
+
+    //Global section
+    background(40) //Sets the background color for the project
+    fill(200) //Sets the color for all of the text.
+    //Time and date section
+    textSize(windowHeight / 7.976) //Defines size for the text of the time and date
+    textAlign(CENTER, BOTTOM) //Aligns the text
+    text(h + ":" + m + ":" + s, 0, (windowHeight/2), width) //Displays the current time
+    textAlign(CENTER, BASELINE)
+    text(d + " / " + mo + " / " + year(), 0, (windowHeight/2), width) //Displays the current date
+    //Timer section
+    textSize(windowHeight / 39.88) //Sizes the text for the timer part
+    textAlign(CENTER, BOTTOM); //Aligns the text for the counter part
+    //console.log(windowHeight + height)
+    text("To show you how much time you've spent, I have this timer " + ~~(millis() / 1000), 0, windowHeight, width) //Displays the timer
+}
