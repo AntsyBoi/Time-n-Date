fae6470d28ee1b599ea2d7a584143aaa6fd12808
AntsyBoi <38162305+AntsyBoi@users.noreply.github.com>
2021-08-20 12:22:21 -0500

    updated index.js so the default port is 80

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 80
 
 app.use(express.static('src'))
 app.use(express.static('node_modules/p5/lib/'))
