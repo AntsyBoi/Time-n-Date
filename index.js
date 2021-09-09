bc5311667436db04b4480ef4ac4f73f8a28f39f2
Ant <38162305+AntsyBoi@users.noreply.github.com>
2021-09-09 17:08:16 -0500

    added https i think

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+const fs = require('fs')
+const https = require('https')
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 80
-
+https.createServer([], app).listen(443)
 app.use(express.static('src'))
 app.use(express.static('node_modules/p5/lib/'))
 app.listen(port, () => {
   console.log(`Time&Date listening at http://localhost:${port}`)
 })
+const httpsOptions = {
+  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem')),
+  key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.key'))
+}
+
+https.createServer(httpsOptions, app)
