b5cf7dd17fab83ad35626fbf2a1f8ab310a27834
Ant <38162305+AntsyBoi@users.noreply.github.com>
2021-10-05 21:31:03 -0500

    [2.0.0 Release] A finally justified 2.0 release
    Fully added optional https and notes
    
    - There are now (I think) pretty in depth notes about everything, it makes it simpler for beginners to read it.
    
    - I added an option to have use HTTPS instead of HTTP. To use HTTP instead of HTTPS, set httpsMode to false.
    
    - Updated the README.md file to include the information on how to set httpsMode
    
    - Made sure keys don't show up in the repository via the gitignore
    
    - Changed version number as I feel this is justified as a 2.0 release

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Time-n-Date
 A simple clock web app made with p5 and node
+
+# How to use
+1. Clone the git repository
+2. Locate the repository in a terminal
+3. Run the command `npm install`
+4. After it's completed, you can tweak the httpsMode variable inside the index.js file to choose weather to use HTTPS or HTTP (HTTPS keys required)
+5. Once everything is configured and setup to your liking run `npm start` to start the script and launch the website
--- index.js
+++ index.js
@@ -1,18 +1,28 @@
+// Imports libraries
 const fs = require('fs')
+const http = require('http')
 const https = require('https')
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = process.env.PORT || 80
-https.createServer([], app).listen(443)
-app.use(express.static('src'))
-app.use(express.static('node_modules/p5/lib/'))
-app.listen(port, () => {
-  console.log(`Time&Date listening at http://localhost:${port}`)
-})
-const httpsOptions = {
-  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'cert.pem')),
-  key: fs.readFileSync(path.join(__dirname, 'ssl', 'key.key'))
-}
 
-https.createServer(httpsOptions, app)
+let port = process.env.PORT || 443 // Sets Default port
+
+let httpsMode = true // Set to false if you want to use HTTP instead of HTTPS
+
+app.use(express.static('src')) // Hosts all the files in the source folder
+app.use(express.static('node_modules/p5/lib/')) // Allows access of the P5js library from the root folder
+if(httpsMode === true){
+  const sslOptions = {
+    key: fs.readFileSync('key.key'), // Sets the location of the Private key
+    cert: fs.readFileSync('cert.pem') // Sets the location of the certificate
+  }; // If httpsMode is false, it sets sslOptions to state where the keys are located and allows use of them later on
+  https.createServer(sslOptions, app).listen(443, () => {
+    console.log(`Time&Date listening at port ${port}`) // Outputs to the console that "Time&Date" (The name of the app) is listening to requests at a certain port
+  }) //
+} else{ // End of HTTPS server, beginning of HTTP server
+  port = process.env.PORT || 80 // Redefines the default port
+  http.createServer(app).listen(443, () => { // Creates HTTP server
+    console.log(`Time&Date listening at http://localhost:${port}`) // Outputs to the console that "Time&Date" (The name of the app) is listening to requests at a certain port
+  }) // End of HTTP server
+}
