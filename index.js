3ef0b5b3168e3eca84628b670cb0d30458d6ea8f
Ant <38162305+AntsyBoi@users.noreply.github.com>
2021-10-08 21:52:46 -0500

    (2.1.0 Release) Tweaks and some fixes
    - Swapped how the clock gets it's time from p5's default time function to JS's built in function for getting the time
    
    - Removed the 0's before the numbers in the date as it felt unnecessary and weird
    
    - Fixed the server not actually not using HTTPS when httpsMode is set to false.
    
    - Changed how the server changes ports for httpsMode
    
    - Removed unnecessary blank comment on the server

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ if(httpsMode === true){
     key: fs.readFileSync('key.key'), // Sets the location of the Private key
     cert: fs.readFileSync('cert.pem') // Sets the location of the certificate
   }; // If httpsMode is false, it sets sslOptions to state where the keys are located and allows use of them later on
-  https.createServer(sslOptions, app).listen(443, () => {
+  https.createServer(sslOptions, app).listen(port, () => {
     console.log(`Time&Date listening at port ${port}`) // Outputs to the console that "Time&Date" (The name of the app) is listening to requests at a certain port
-  }) //
+  })
 } else{ // End of HTTPS server, beginning of HTTP server
   port = process.env.PORT || 80 // Redefines the default port
-  http.createServer(app).listen(443, () => { // Creates HTTP server
+  http.createServer(app).listen(port, () => { // Creates HTTP server
     console.log(`Time&Date listening at http://localhost:${port}`) // Outputs to the console that "Time&Date" (The name of the app) is listening to requests at a certain port
   }) // End of HTTP server
 }
